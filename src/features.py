5d161d2f8cfc7078b9212f91b85b3f00a2aa0132
jinheonyoon <yjinheon@gmail.com>
2021-12-14 21:10:36 +0900

    working in progress

--- src/features.py
+++ src/features.py
@@ -35,7 +35,12 @@ get_demo(df):
 
 # 사용근로자 관련 feature 리스트
 
-feat_list = [
+feature_list = [
+        'g181d001', # turnover_exp
+        'g181d006', # work_exp
+        'g181e001', # 전직장 경험
+       'g181a021', # 상용근로자 여부
+       'g181sq006', # 사용근로자 여부
        'g181pid', # id 
        'g181majorcat', # 전공계열
        'g181sex', # 성별 
@@ -119,14 +124,96 @@ feat_list = [
 ]
 
 
+# dictionary for converting feature id to feature name
+
+feature_dict =  {
+      'g181pid':'id', # id 
+       'g181majorcat':'majorcat_cat', # 전공계열
+       'g181sex':'sex_cat', # 성별   
+       'g181birthy':'birth_date', # 출생년
+       'g181age':'age_num', # 연령
+       'g181graduy':'graduy_date', #졸업년
+### 사업체 관련       
+        'g181a001':'year_start_date', # 현일자리 시작년
+        'g181a002':'month_start_date', # 현일자리 시작월
+        'g181a004_10':'ind_cat', # 일자리 산업 대분류
+        'g181a010':'corp_worker_num', # 기업체 종사자 수
+        'g181a011':'biz_worker_cat', # 사업체 종사자 수
+        'g181a018':'tw_hour', # 출근시간_시간
+        'g181a019':'tw_min', # 출근시간_분
+        'g181a116':'workday_num', # 주당 정규 근로일
+        'g181a117':'worktime_num', # 주당정규 근로시간
+        'g181a118':'worktime_ex_num', # 주당초과 근로시간
+        'g181a119': 'holy_work_num', # 월평균 휴일근로
+        'g181a020': 'biztype_cat', # 사업체형태
+       'g181a022':'regular_cat',     # 정규직 비정규직 여부
+### 혜택관련
+        'g181a390':'voluntary_cat',     # 일자리 형태 자발, 비자발 여부
+        'g181a035':'shift_cat' ,    # 교대제 여부
+        'g181a038':'pension_cat',     # 퇴직금 제공 여부
+        'g181a039':'payed_vc_cat',     # 제공여부 2- 유급휴가
+        'g181a043':'maternitylv_cat',     #  6- 육아휴직
+        'g181a045':'overtime_pay_cat',     # 8- 시간 외 수당
+        'g181a046':'bonus_cat',     # 9- 상여금
+        'g181a048':'weely_hl_cat',     # 11- 유급주휴
+        'g181a392':'baby_vc_cat',     # 12- 산전후휴가
+        'g181a120':'wage_type_cat',     # 급여 형태 구분
+        'g181a122':'month_wage_num',     # 월 평균 근로소득
+        'g181a126':'sat_wage_num',     # 만족도-임금
+        'g181a127':'sat_stable_num',     # 만족도-고용안정성
+        'g181a128':'sat_work_num',     # 만족도-직무내용
+        'g181a129':'sat_envi_num',     # 만족도-근무환경
+        'g181a130':'sat_wt_num',     # 만족도-노동시간
+        'g181a131':'sat_potential_num',     # 만족도-발전가능성
+        'g181a132':'sat_relation_num',     # 만족도-인간관계
+        'g181a133':'sat_welfare_num',     # 만족도-복리후생
+        'g181a134':'sat_hr_num',     # 만족도-인사체계
+        'g181a135':'sat_rep1_num',     # 만족도-사회적평판-일
+        'g181a136':'sat_auto_num',     # 만족도-자율성 및 권한
+        'g181a137':'sat_rep2_num',     # 만족도-일자리-사회적 평판
+        'g181a138':'sat_fit_num',     # 만족도-적성흥미일치도
+        'g181a139':'sat_edu_num',     # 만족도-직무관련 교육
+        'g181a140':'sat_general_num',     # 만족도-일자리_전반적만족도
+        'g181a141':'sat_work-general_num',     # 만족도-업무_전반적만족도
+        'g181a142':'edu-fit_num',     # 교육수준-일수준일치정도
+        'g181a143':'skill-fit_num',     # 일기술수준-본인기술수준일치정도
+        'g181a144':'major-fit_num',     # 주전공일치정도
+        'g181a146':'major_help_num',     # 전공지식업무도움정도
+        'g181a158':'ins_1_num',     # 보험-국민연금
+        'g181a159':'ins_2_num',     # 보험-특수직역연금
+        'g181a160':'ins_3_num',     # 보험-건강보험
+        'g181a161':'ins_4_num',     # 보험-고용보험
+        'g181a162':'ins_5_num',     # 보험-산재보험
+        'g181a189':'seeking_time_num',     # 구직활동경험기간-개월
+        'g181a283':'adjust_difficulty_cat',     # 다른일자리제의여부
+        'g181a285':'job_offer_cat',     # 적응시어려움여부
+        'g181a297':'turnover_intention',     # 이직준비 여부: target
+        'g181g001':'graduate_cat',     # 대학원 경험유무
+        'g181l001':'train_cat',     # 취업훈련경험유무
+        'g181q001':'health_num',     # 현재 견강상태
+        'g181q004':'smoke_cat',     # 흡연여부
+        'g181q006':'drink_num,',     # 음주빈도
+        'g181q015':'lifesat_personal',     # 삶의만족도-개인적 측면
+        'g181q016':'lifesat_relational',     # 삶의만족도-관계적 측면
+        'g181q017':'lifesat_group',     # 삶의만족도-소속집단
+        'g181q018':'emg_joy_num',     # 감정빈도-즐거운
+        'g181q019':'emg_happy_num',     # 감정빈도-행복한
+        'g181q020':'emg_comfort_num',     # 감정빈도-편안한
+        'g181q021':'emb_irr_num',     # 감정빈도-짜증나는
+        'g181q022':'emb_negative_num',     # 감정빈도-부정적인
+        'g181q023':'emb_spiritless',     # 감정빈도-무기력한
+        'g181p001':'marriage_cat',     # 혼인여부
+        'g181p008':'child_cat',     # 부양자녀 유무
+        'g181p036':'parent_asset_cat',     # 부모님 자산규모
+        'g181p046':'livetype_cat',     # 거주형태
+        'g181p041':'support_cat'      # 경제적 지원여부
+}
+
 # subset data
 
 def subset_df(df):
-    
-    worker_df=df[col_list]
-    
-    worker_df = df[df['g181sq006'].notnull()] # 근로자(일의 종류)
-    
+    worker_df = df[feature_list]
+    worker_df = worker_df[worker_df['g181sq006'].notnull()] # 근로자(일의 종류)
     
     # 임금종사자 여부
     """
@@ -144,6 +231,9 @@ def subset_df(df):
     
     worker_df['worker_type'] = np.select(worker_cond, worker_choices,default=None)
     
+    
+    worker_df.drop(['g181sq006'], axis=1, inplace=True)
+    
     # 임금근로자만 추출
     worker_df = worker_df[worker_df['worker_type'] == 'employed'] 
     
@@ -160,6 +250,7 @@ def subset_df(df):
     worker_df['regular_worker'] = np.select(regular_cond, ['yes','no'],default=None)
     worker_df = worker_df[worker_df['regular_worker'] == 'yes']
     
+    worker_df.drop(['g181a021'], axis=1, inplace=True)
     
     return worker_df
 
@@ -176,14 +267,19 @@ def engineer(df):
 
     df['turnover_exp'] = np.select(turnover_cond, choices,default=3)
     
+    df.drop(['g181d001','g181d006','g181e001'], axis=1, inplace=True)
+
     # 알바이외 근로경험 여부
-    df['work_exp'] = np.where(df['turnover_ex'].isin([0,1]),0,1)
+    df['work_exp'] = np.where(df['turnover_exp'].isin([0,1]),0,1)
     
+    # 구직활동기간 결측값 0으로 처리
+    df['g181a189'] = df['g181a189'].fillna(0)
+    # feature name 변경 (map)
+    df.rename(columns=feature_dict, inplace=True)
     
-    
+    #df.columns = df.columns.map(feature_dict)
 
-
-
+    return df
 
 
 
@@ -197,7 +293,7 @@ def engineer(df):
         
         
         
-        
+"""        
 data = data.drop(config["drop_columns"], axis=1)
 # Define X (independent variables) and y (target variable)
 X = np.array(data.drop(config["target_name"], 1))
@@ -206,3 +302,4 @@ y = np.array(data[config["target_name"]])
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=config["test_size"], random_state= config["random_state"])
 
+"""
